14bf28bef3c881b397bb2b8df2703220584706ac
zuoxinyu <iamzuoxinyu@gmail.com>
2019-03-17 16:57:41 +0800

    jzon

--- jzon/src/jzon.rs
+++ jzon/src/jzon.rs
@@ -1,7 +1,7 @@
 #[allow(dead_code)]
-use std::mem;
 use std::string::String;
 use std::vec::Vec;
+use std::mem;
 
 #[derive(Debug)]
 pub enum Jzon {
@@ -28,6 +28,10 @@ impl ParseErr {
     pub fn new() -> ParseErr {
         ParseErr::ParseError(String::from("parse error"))
     }
+
+    pub fn expect(str: &str) -> ParseErr {
+        ParseErr::ParseError(String::from(str))
+    }
 }
 
 pub type ParseResult<T> = Result<T, ParseErr>;
@@ -35,10 +39,10 @@ pub type ParseResult<T> = Result<T, ParseErr>;
 impl Jzon {
     pub fn parse(bytes: &[u8]) -> ParseResult<Jzon> {
         match bytes[0] as char {
-            ' ' | '\t' | '\r' | '\n'
-                => Jzon::parse(&bytes[1..]),
-            '-' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0'
-                => Jzon::parseNumber(bytes),
+            ' ' | '\t' | '\r' | '\n' => Jzon::parse(&bytes[1..]),
+            '-' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' => Jzon::parseNumber(
+                bytes,
+            ),
             't' => Jzon::parseTrue(bytes),
             'f' => Jzon::parseFalse(bytes),
             'n' => Jzon::parseNull(bytes),
@@ -98,7 +102,7 @@ impl Jzon {
         loop {
             s.push(match remain_bytes[0] as char {
                 '\\' => Jzon::parseEscapedChar(&bytes[1..])?,
-                ch   => ch
+                ch => ch,
             });
         }
     }
@@ -106,26 +110,99 @@ impl Jzon {
     #[allow(non_snake_case)]
     fn parseEscapedChar(bytes: &[u8]) -> Result<char, ParseErr> {
         Ok(match bytes[1] as char {
-            'b'  => 08 as char,
-            't'  => '\t',
-            'n'  => '\n',
-            'r'  => '\r',
-            '"'  => '\"',
-            '/'  => '/',
+            'b' => 08 as char,
+            't' => '\t',
+            'n' => '\n',
+            'r' => '\r',
+            '"' => '\"',
+            '/' => '/',
             '\\' => '\\',
-            'u'  => Jzon::parseUnicodePoint(&bytes[1..])?,
-            _    => return Err(ParseErr::new()), // the `return` expression is type of `!` which is the subtype of all other types
+            'u' => Jzon::parseUnicodePoint(&bytes[1..])?,
+            _ => return Err(ParseErr::new()),
+            // the `return` expression is type of `!` which is the subtype of all other types
         })
     }
 
     #[allow(non_snake_case)]
     fn parseUnicodePoint(bytes: &[u8]) -> Result<char, ParseErr> {
-        Err(ParseErr::new())
+        fn invalid(cp: u32) -> bool { 0xDC00 <= cp && cp <= 0xDFFF || cp == 0 }
+        let mut uc = Jzon::parseHex4(&bytes[0..4])?;
+        if invalid(uc) {
+            return Err(ParseErr::expect("invalid codepoint"));
+        }
+
+        if 0xD800 <= uc && uc <= 0xDBFF {
+            if !(bytes[4] == b'\\' && bytes[5] == b'u') {
+                return Err(ParseErr::expect("need succeed codepoint"));
+            }
+
+            let uc2 = Jzon::parseHex4(&bytes[6..10])?;
+            if invalid(uc2) {
+                return Err(ParseErr::expect("invalid codepoint"));
+            }
+
+            uc = 0x10000 + (((uc&0x3FF) << 10 | uc2) & 0x3FF);
+        }
+
+        let len = match uc {
+            uc if uc < 0x80 => 1,
+            uc if uc < 0x800 => 2,
+            uc if uc < 0x10000 => 3,
+            _ => 4
+        };
+        let mut parsed : [u8;4] = [0,0,0,0];
+        let firstByteMarkMap = [0x00, 0x00, 0xC0, 0xE0, 0xF0];
+        for i in 1..len {
+            parsed[i] = ((uc | 0x80) & 0xBF) as u8; uc >>= 6;
+        }
+        parsed[0] = (uc | firstByteMarkMap[len]) as u8;
+
+        /*
+        match len {
+            4 => {parsed[3] = (uc | 0x80) & 0xBF; uc >>= 6;},
+            3 => {parsed[2] = (uc | 0x80) & 0xBF; uc >>= 6;},
+            2 => {parsed[1] = (uc | 0x80) & 0xBF; uc >>= 6;},
+            1 => {parsed[0] = (uc | firstByteMarkMap[len]);},
+            _ => (),
+        };
+        */
+
+        let mut realParsed : [u8;4] = [0,0,0,0];
+        for (i, c) in parsed.iter().enumerate() {
+            if (*c) != 0u8 {
+                realParsed[i] = *c;
+            }
+        }
+
+
+        let mut u : u32 = 0;
+        unsafe {
+            u = mem::transmute::<[u8;4], u32>(realParsed);
+        }
+
+        return Err(ParseErr::expect(""));
     }
 
     #[allow(non_snake_case)]
-    fn parseHex4(bytes: &[u8]) -> Result<char, ParseErr> {
-        Err(ParseErr::new())
+    fn parseHex4(bytes: &[u8]) -> Result<u32, ParseErr> {
+        bytes[0..4].iter().enumerate().fold(
+            Ok(0u32),
+            |init, (i, ch)| {
+                (*ch as char)
+                    .to_digit(16)
+                    .ok_or(ParseErr::expect("invalid hex digit character"))
+                    .map(|res| init.unwrap() + res * (0x1000u32 >> (i * 4)))
+            },
+        )
     }
 }
 
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_hex() {
+        let bytes: [u8; 4] = [b'4', b'e', b'2', b'd'];
+        let res = Jzon::parseHex4(&bytes);
+        assert_eq!(0x4e2d, res.unwrap());
+    }
+}
